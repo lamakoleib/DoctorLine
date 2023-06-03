38f6408dfc12c963415ef69f65b7c66e5b12cbee
Lama Koleib <lkole095@uottawa.ca>
2023-06-03 16:34:16 -0400

    Implemented Services section and fixed minor issue

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const parentContainer = document.querySelector(".card");
 
+
+var card = document.getElementById("doctor-1");
+var button = document.getElementById("see-1");
+
+button.onclick = function(){
+
+    if(card.className=="open"){
+        card.className="card mb-3";
+        button.innerHTML = "See More";
+    }
+
+    else{
+        card.className="open";
+        button.innerHTML = "See Less";
+    }
+};
