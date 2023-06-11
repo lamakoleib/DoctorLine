237838305415ec211a81eb2c9282f2aec35870cf
Lama Koleib <lkole095@uottawa.ca>
2023-06-11 19:20:44 -0400

    Minor organization for code

--- script.js
+++ script.js
@@ -5,20 +5,9 @@ var wrapper = document.getElementsByClassName("form-wrapper")[0]
 var form_first = document.getElementsByClassName("form-first")[0]
 var form_second = document.getElementsByClassName("form-second")[0]
 var backBtn = document.getElementsByClassName("back-btn")[0]
-var bookBtn = document.getElementById("book-button")
 
-// var nextBtn = form.querySelector(".next-btn")
-// var allInput = form.querySelectorAll(".first input")
 
-// button.onclick = function () {
-//   if (card.className == "open") {
-//     card.className = "card mb-3"
-//     button.innerHTML = "See More"
-//   } else {
-//     card.className = "open"
-//     button.innerHTML = "See Less"
-//   }
-// }
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
@@ -37,25 +26,8 @@ backBtn.addEventListener("click", () => {
   wrapper.classList.add("wrapper-slide-right")
 })
 
-bookBtn.addEventListener("click",()=>{
-   
-   
-    
-
-})
 
 
 
-// nextBtn.addEventListener =
-//   ("click",
-//   () => {
-//     allInput.forEach((input) => {
-//       if (input.value != "") {
-//         form.classList.add("active")
-//       } else {
-//         form.classList.remove("active")
-//         alert("empty")
-//       }
-//     })
-//   })
+
 
